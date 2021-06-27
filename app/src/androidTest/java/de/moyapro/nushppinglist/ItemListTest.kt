af48a3776fff9299038f5bd4da27d41699a9eb5f
Tom Ritmeier <tom.ritmeier@gmail.com>
2021-06-27 11:02:18 +0200

    switch to edit item mode

--- app/src/androidTest/java/de/moyapro/nushppinglist/ItemListTest.kt
+++ app/src/androidTest/java/de/moyapro/nushppinglist/ItemListTest.kt
@@ -4,6 +4,7 @@ import androidx.compose.ui.test.assertHasClickAction
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
 import de.moyapro.nushppinglist.ui.ItemList
 import de.moyapro.nushppinglist.ui.ItemListElement
 import de.moyapro.nushppinglist.ui.theme.NuShppingListTheme
@@ -51,9 +52,19 @@ internal class ItemListTest {
     @Test
     fun clickableItemList() {
         val name = "Milk"
-        createComposable(listOf(Item(name)))
-        val onNodeWithText = composeTestRule.onNodeWithText(name)
-        onNodeWithText.assertHasClickAction()
+        createComposable(Item(name))
+        val itemNode = composeTestRule.onNodeWithText(name)
+        itemNode.assertHasClickAction()
+    }
+
+    @Test
+    fun switchToEditItem() {
+        val name = "Milk"
+        createComposable(Item(name))
+        val itemNode = composeTestRule.onNodeWithText(name)
+        composeTestRule.onNodeWithText("Save").assertDoesNotExist()
+        itemNode.performClick()
+        composeTestRule.onNodeWithText("Save").assertIsDisplayed()
     }
 
 }
--- app/src/main/java/de/moyapro/nushppinglist/ui/ItemListElement.kt
+++ app/src/main/java/de/moyapro/nushppinglist/ui/ItemListElement.kt
@@ -2,18 +2,26 @@ package de.moyapro.nushppinglist.ui
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import de.moyapro.nushppinglist.Item
 
 @Composable
 fun ItemListElement(item: Item) {
-
+    var isEdited: Boolean by remember {
+        mutableStateOf(false)
+    }
     Text(
         item.name,
         modifier = Modifier
             .fillMaxWidth()
-            .clickable(onClick = {})
+            .clickable(onClick = { isEdited = !isEdited })
     )
+    if (isEdited) {
+        Button(onClick = { isEdited = !isEdited }) {
+            Text("Save")
+        }
+    }
 }
