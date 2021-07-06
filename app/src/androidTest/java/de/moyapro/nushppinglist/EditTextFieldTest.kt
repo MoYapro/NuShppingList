0655624794f164b1a5f7f7984939a70a1c15eff3
Tom Ritmeier <tom.ritmeier@gmail.com>
2021-07-06 08:37:11 +0200

    test text input

--- app/src/androidTest/java/de/moyapro/nushppinglist/EditTextFieldTest.kt
+++ app/src/androidTest/java/de/moyapro/nushppinglist/EditTextFieldTest.kt
@@ -3,8 +3,10 @@ package de.moyapro.nushppinglist
 import androidx.compose.ui.test.assertCountEquals
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onAllNodesWithContentDescription
+import androidx.compose.ui.test.performTextInput
 import de.moyapro.nushppinglist.ui.EditTextField
 import de.moyapro.nushppinglist.ui.theme.NuShppingListTheme
+import org.junit.Assert.assertEquals
 import org.junit.Rule
 import org.junit.Test
 
@@ -14,15 +16,21 @@ class EditTextFieldTest {
     val composeTestRule = createComposeRule()
 
     @Test
-    fun textEditIsFound() {
-        var value = "1"
+    fun textEditIsEditable() {
+        val initialValue = "original"
+        var value = initialValue
+        val textInput = "input"
         composeTestRule.setContent {
             NuShppingListTheme {
-                EditTextField("foobar", value) { x -> value = x }
+                EditTextField("Label", initialValue) { x -> value = x }
             }
         }
-        composeTestRule.onAllNodesWithContentDescription(EditTextField.DESCRIPTION)
-            .assertCountEquals(1)
+        val fields =
+            composeTestRule.onAllNodesWithContentDescription(EditTextField.DESCRIPTION)
+        fields.assertCountEquals(1)
+        val editField = fields[0]
+        editField.performTextInput(textInput)
+        assertEquals("Should update initialValue", initialValue + textInput, value)
     }
 
 }
