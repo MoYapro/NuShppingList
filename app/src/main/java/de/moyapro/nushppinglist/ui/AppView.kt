1cb4a1e26001a74ac410719c6ef1cddaddbd0839
Tom Ritmeier <tom.ritmeier@gmail.com>
2021-07-26 13:38:47 +0200

    improve main layout + switch

--- app/src/main/java/de/moyapro/nushppinglist/ui/AppView.kt
+++ app/src/main/java/de/moyapro/nushppinglist/ui/AppView.kt
@@ -1,25 +1,53 @@
 package de.moyapro.nushppinglist.ui
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
 import de.moyapro.nushppinglist.VM
+import de.moyapro.nushppinglist.ui.theme.Purple700
 
 @Composable
 fun AppView(viewModel: VM, showCart: Boolean = true) {
     var cartIsDisplayed: Boolean by remember { mutableStateOf(showCart) }
     Column {
 
-        Button(onClick = {
-            cartIsDisplayed = !cartIsDisplayed
-        }) {
-            Text("XXX")
-        }
+        cartIsDisplayed = ViewSelector(cartIsDisplayed) { newValue -> cartIsDisplayed = newValue }
 
         if (cartIsDisplayed)
             CartView(viewModel)
         else
             ItemView(viewModel)
     }
+}
+
+@Composable
+private fun ViewSelector(cartIsDisplayed: Boolean, setNewState: (Boolean) -> Unit): Boolean {
+    var cartIsDisplayed1 = cartIsDisplayed
+    Row {
+        Button(
+            modifier = Modifier
+                .fillMaxWidth(.5F),
+            colors = ButtonDefaults.buttonColors(backgroundColor = if (cartIsDisplayed1) Purple700 else Color.Gray),
+            onClick = {
+                setNewState(true)
+            }) {
+            Text("Einkaufsliste")
+        }
+        Spacer(modifier = Modifier.width(Dp(4F)))
+        Button(
+            modifier = Modifier
+                .fillMaxWidth(),
+            colors = ButtonDefaults.buttonColors(backgroundColor = if (!cartIsDisplayed1) Purple700 else Color.Gray),
+            onClick = {
+                setNewState(false)
+            }) {
+            Text("Dinge")
+        }
+    }
+    return cartIsDisplayed1
 }
--- app/src/main/java/de/moyapro/nushppinglist/ui/CartItemList.kt
+++ app/src/main/java/de/moyapro/nushppinglist/ui/CartItemList.kt
@@ -3,11 +3,14 @@ package de.moyapro.nushppinglist.ui
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
 import de.moyapro.nushppinglist.CartItemProperties
 import de.moyapro.nushppinglist.VM
 
@@ -21,6 +24,9 @@ fun CartListElement(
         Row(
             Modifier.fillMaxWidth(),
         ) {
+            Spacer(modifier = Modifier.width(Dp(4F)))
+            Text(text = cartItem.amount.toString())
+            Spacer(modifier = Modifier.width(Dp(4F)))
             Text(
                 text = item.name,
                 modifier = Modifier
