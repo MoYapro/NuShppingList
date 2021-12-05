4a1fdcc1cfe72df0480baba5a384a6f9a5ba8f30
Tom Ritmeier <tom.ritmeier@iits-consulting.de>
2021-12-05 08:25:04 +0100

    improve RecipeDbTest.kt

--- app/src/androidTest/java/de/moyapro/nushppinglist/db/RecipeDbTest.kt
+++ app/src/androidTest/java/de/moyapro/nushppinglist/db/RecipeDbTest.kt
@@ -5,6 +5,7 @@ import de.moyapro.nushppinglist.db.dao.RecipeDao
 import de.moyapro.nushppinglist.db.model.Recipe
 import de.moyapro.nushppinglist.ui.model.RecipeViewModel
 import de.moyapro.nushppinglist.ui.util.createSampleRecipeCake
+import de.moyapro.nushppinglist.ui.util.createSampleRecipeNoodels
 import de.moyapro.nushppinglist.util.DbTestHelper
 import io.kotest.matchers.collections.shouldContainExactlyInAnyOrder
 import io.kotest.matchers.collections.shouldHaveSize
@@ -38,31 +39,33 @@ class RecipeDbTest {
 
     @Test(timeout = 10000)
     @Throws(Exception::class)
-    fun writeAndLoadRecipe() = runBlocking {
-        val recipe = createSampleRecipeCake(recipeId = 1)
-        val recipe2 = createSampleRecipeCake(recipeId = 2)
+    fun writeAndLoadRecipe(): Unit = runBlocking {
+        val recipeCake = createSampleRecipeCake(recipeId = 1)
+        val recipeNoodles = createSampleRecipeNoodels(recipeId = 2)
 
-        recipeViewModel.save(recipe, recipe2)
+        recipeViewModel.save(recipeCake, recipeNoodles)
 
-        val dbRecipe: Recipe = recipeDao.findAllRecipe().first().first()
+        val dbRecipe: Recipe = recipeDao.findAllRecipe().first().single { it.recipeId == recipeCake.recipeId }
 
-        dbRecipe.recipeId shouldBe recipe.recipeId
-        dbRecipe.recipeProperties.description shouldBe recipe.recipeProperties.description
-        dbRecipe.recipeProperties.title shouldBe recipe.recipeProperties.title
+        dbRecipe.recipeId shouldBe recipeCake.recipeId
+        dbRecipe.recipeProperties.description shouldBe recipeCake.recipeProperties.description
+        dbRecipe.recipeProperties.title shouldBe recipeCake.recipeProperties.title
         dbRecipe.recipeItems shouldHaveSize 2
-        dbRecipe.recipeItems.map { it.recipeItemId } shouldContainExactlyInAnyOrder recipe.recipeItems.map { it.recipeItemId }
-        dbRecipe.recipeItems.map { it.recipeId } shouldContainExactlyInAnyOrder recipe.recipeItems.map { it.recipeId }
-        dbRecipe.recipeItems.map { it.amount } shouldContainExactlyInAnyOrder recipe.recipeItems.map { it.amount }
-        dbRecipe.recipeItems.map { it.item.itemId } shouldContainExactlyInAnyOrder recipe.recipeItems.map { it.item.itemId }
+        dbRecipe.recipeItems.map { it.recipeItemId } shouldContainExactlyInAnyOrder recipeCake.recipeItems.map { it.recipeItemId }
+        dbRecipe.recipeItems.map { it.recipeId } shouldContainExactlyInAnyOrder recipeCake.recipeItems.map { it.recipeId }
+        dbRecipe.recipeItems.map { it.amount } shouldContainExactlyInAnyOrder recipeCake.recipeItems.map { it.amount }
+        dbRecipe.recipeItems.map { it.item.itemId } shouldContainExactlyInAnyOrder recipeCake.recipeItems.map { it.item.itemId }
         dbRecipe.recipeSteps shouldHaveSize 2
-        dbRecipe.recipeSteps.map { it.recipeId } shouldContainExactlyInAnyOrder recipe.recipeSteps.map { it.recipeId }
-        dbRecipe.recipeSteps.map { it.recipeStepId } shouldContainExactlyInAnyOrder recipe.recipeSteps.map { it.recipeStepId }
-        dbRecipe.recipeSteps.map { it.stepNumber } shouldContainExactlyInAnyOrder recipe.recipeSteps.map { it.stepNumber }
-        dbRecipe.recipeSteps.map { it.stepDescription } shouldContainExactlyInAnyOrder recipe.recipeSteps.map { it.stepDescription }
+        dbRecipe.recipeSteps.map { it.recipeId } shouldContainExactlyInAnyOrder recipeCake.recipeSteps.map { it.recipeId }
+        dbRecipe.recipeSteps.map { it.recipeStepId } shouldContainExactlyInAnyOrder recipeCake.recipeSteps.map { it.recipeStepId }
+        dbRecipe.recipeSteps.map { it.stepNumber } shouldContainExactlyInAnyOrder recipeCake.recipeSteps.map { it.stepNumber }
+        dbRecipe.recipeSteps.map { it.stepDescription } shouldContainExactlyInAnyOrder recipeCake.recipeSteps.map { it.stepDescription }
 
-        dbRecipe shouldBe recipe
+        dbRecipe.recipeId shouldBe recipeCake.recipeId
+        dbRecipe.recipeProperties shouldBe recipeCake.recipeProperties
+        dbRecipe.recipeItems shouldContainExactlyInAnyOrder recipeCake.recipeItems
+        dbRecipe.recipeSteps shouldContainExactlyInAnyOrder recipeCake.recipeSteps
 
-        Unit
     }
 
 
