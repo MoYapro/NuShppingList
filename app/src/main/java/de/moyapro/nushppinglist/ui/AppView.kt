fbd15c437d19ef3bc919da940f3ae16938af48cf
Tom Ritmeier <tom.ritmeier@iits-consulting.de>
2021-12-19 08:41:38 +0100

    use tabrow for selecting current view

--- app/src/main/java/de/moyapro/nushppinglist/ui/AppView.kt
+++ app/src/main/java/de/moyapro/nushppinglist/ui/AppView.kt
@@ -1,10 +1,10 @@
 package de.moyapro.nushppinglist.ui
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Tab
+import androidx.compose.material.TabRow
+import androidx.compose.material.Text
 import androidx.compose.runtime.*
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import de.moyapro.nushppinglist.ui.MainView.*
 import de.moyapro.nushppinglist.ui.model.CartViewModel
 import de.moyapro.nushppinglist.ui.model.RecipeViewModel
@@ -15,11 +15,23 @@ fun AppView(
     recipeViewModel: RecipeViewModel,
     selectedView: MainView = EINKAUFSLISTE,
 ) {
+
+    var state by remember { mutableStateOf(0) }
     var displayedView by remember { mutableStateOf(selectedView) }
-    Column(Modifier.background(color = Color.Magenta)) {
 
-        ViewSelector(displayedView) { newValue -> displayedView = newValue }
-
+    Column() {
+        TabRow(selectedTabIndex = state) {
+            values().forEachIndexed { index, mainView ->
+                Tab(
+                    text = { Text(mainView.text) },
+                    selected = state == index,
+                    onClick = {
+                        state = index
+                        displayedView = mainView
+                    }
+                )
+            }
+        }
         when (displayedView) {
             EINKAUFSLISTE -> CartView(cartViewModel)
             DINGE -> ItemView(cartViewModel)
@@ -27,4 +39,5 @@ fun AppView(
         }
     }
 }
+
 
