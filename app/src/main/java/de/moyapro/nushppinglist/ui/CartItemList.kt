c91c5af422d5b70a81949208d6841a3aec8535d6
Tom Ritmeier <tom.ritmeier@iits-consulting.de>
2021-12-25 10:34:41 +0100

    improved checked display

--- app/src/main/java/de/moyapro/nushppinglist/ui/CartItemList.kt
+++ app/src/main/java/de/moyapro/nushppinglist/ui/CartItemList.kt
@@ -3,7 +3,11 @@ package de.moyapro.nushppinglist.ui
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
+import androidx.compose.material.contentColorFor
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CheckCircle
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -18,37 +22,42 @@ fun CartListElement(
     cartItem: CartItem,
     viewModel: CartViewModel,
 ) {
+    val backgroundColor = getBackgroundColor(cartItem.cartItemProperties.checked)
+    val textColor = contentColorFor(backgroundColor)
     Row(
         Modifier
             .fillMaxWidth()
             .clickable(onClick = { viewModel.toggleChecked(cartItem.cartItemProperties) })
-            .background(getBackgroundColor(cartItem.cartItemProperties.checked)),
+            .background(backgroundColor),
         horizontalArrangement = Arrangement.SpaceBetween
     ) {
         Row(modifier = Modifier.fillMaxWidth(.8F)) {
             KategoryIndicator(cartItem.item)
             Spacer(modifier = Modifier.width(Dp(4F)))
-            Text(text = cartItem.cartItemProperties.amount.toString())
+            Text(text = cartItem.cartItemProperties.amount.toString(), color = textColor)
             Spacer(modifier = Modifier.width(Dp(4F)))
-            Text(text = cartItem.item.defaultItemUnit.short)
+            Text(text = cartItem.item.defaultItemUnit.short, color = textColor)
             Spacer(modifier = Modifier.width(Dp(4F)))
+            if (cartItem.cartItemProperties.checked) {
+                Icon(Icons.Filled.CheckCircle, contentDescription = "Gekauft")
+                Spacer(modifier = Modifier.width(Dp(4F)))
+            }
             Column {
-                Text(text = cartItem.item.name)
+                Text(text = cartItem.item.name, color = textColor)
                 if (cartItem.item.description.isNotBlank()) {
-                    Text(text = cartItem.item.description, fontSize= 13.sp)
+                    Text(text = cartItem.item.description, fontSize = 13.sp, color = textColor)
                 }
             }
         }
         Row {
-            Text("${cartItem.item.price} €")
+            Text(text = "${cartItem.item.price} €", color = textColor)
         }
         Spacer(modifier = Modifier.width(Dp(4F)))
-
     }
 }
 
 @Composable
 private fun getBackgroundColor(checked: Boolean) = when (checked) {
-    true -> Color.LightGray
+    true -> Color.DarkGray
     false -> Color.Unspecified
 }
